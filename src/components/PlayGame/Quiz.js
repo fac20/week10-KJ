23dc44eedbe454206da7fe6e7652f14cdfdd89b2
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-09-17 15:25:39 +0100

    feat: add or minus from the progress br depending on users answers

--- src/components/PlayGame/Quiz.js
+++ src/components/PlayGame/Quiz.js
@@ -1,10 +1,12 @@
 // import datahelpers stuff 
 import getData from "../../utils/data_helpers"
 import React from "react"
+import Timer from "./Timer"
 
 export default function Quiz(props) {
     const [jsonData, setJsonData] = React.useState(null)
     const [index, setIndex] = React.useState(0);
+    const [timeLeft, setTimeLeft] = React.useState(15)
 
     console.log("index", index)
     
@@ -20,19 +22,19 @@ export default function Quiz(props) {
     const correct_answer = currentQuestion.correct_answer;
     const incorrect_answers = currentQuestion.incorrect_answers;
     const choices = [correct_answer, ...incorrect_answers]
-   console.log("correct answer" , correct_answer)
+   
     function checkAnswer(event){
         
         // if selected answer = correct answer 
         //  then change background of button to green
         //  else if wrong background of button to red
-
-        // event.target is the clicked button
-        // event.target.value should be words
         if (event.target.textContent === correct_answer) {
             event.target.className = "green";
+            setTimeLeft(timeLeft + 5);
+            
         } else {
             event.target.className = "red";
+            setTimeLeft(timeLeft - 2);
         }
     }
     /*
@@ -46,14 +48,15 @@ export default function Quiz(props) {
 
     return (
         <main>
-
+            <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft}/>
             { index === 0 ? <h1>Welcome, {props.username}!</h1> : null }
             {/* <div dangerouslySetInnerHTML={Quiz()} />; */}
-            <h2>{index}.{question}</h2> 
+            <h2>{index + 1}.{question}</h2> 
             {choices.map(choice => (
                 <button onClick ={(event) =>{
                     checkAnswer(event)
                     setIndex(index + 1)
+
                 }
                 
                 }>{choice}</button>
