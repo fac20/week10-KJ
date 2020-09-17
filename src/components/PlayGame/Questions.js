e77132c0ab1ba3a025821b2cfc2230c7f288a504
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-09-17 10:13:19 +0100

    add steps inpseudocode

---
+++ src/components/PlayGame/Questions.js
@@ -0,0 +1,40 @@
+// import datahelpers stuff 
+import {API_BASE, getData} from "../../utils/data_helpers"
+
+
+
+// create function 
+/*
+- should get animals data
+"results": [
+    {
+      "category": "Animals",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "The Axolotl is an amphibian that can spend its whole life in a larval state.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "category": "Animals",
+      "type": "multiple",
+      "difficulty": "hard",
+      "question": "What was the name of the Ethiopian Wolf before they knew it was related to wolves?",
+      "correct_answer": "Simien Jackel",
+      "incorrect_answers": [
+        "Ethiopian Coyote",
+        "Amharic Fox",
+        "Canis Simiensis"
+      ]
+    },
+- create choices variable - whcih will have true and false answers
+- counter var- tab of how many question  the user has attempted/has been rendered
+- render 2 sections - 1 with questions and another with choices
+- render timer bar which we will import from timer.js
+
+
+
+
+*/
