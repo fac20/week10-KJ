00edf8d81e4aa484f384f2d3ced6deba3744414b
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-09-17 18:02:54 +0100

    Add tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders initial page welcome message', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/start game/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMess = getByText(/how seal-y/i);
+  expect(welcomeMess).toBeInTheDocument();
+});
+
+test('render start game button', () => {
+  const { getByText } = render(<App />);
+  const startButton = getByText(/Start Game/);
+  expect(startButton).toBeInTheDocument();
 });
